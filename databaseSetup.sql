11ab40750418efc51a65f955c356395d6048f5b6
Chase Whitney <ChaseEWhitney@gmail.com>
2017-11-08 13:31:36 -0600

    added db setup

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -1,38 +1,19 @@
-CREATE TABLE owners (
-	id SERIAL PRIMARY KEY,
-	first_name varchar(30) not null,
-	last_name varchar(50) not null
+CREATE TABLE "spells" (
+  "id" serial primary key,
+  "index" integer,
+  "name" varchar(80) not null,
+  "casting_time" varchar(50) not null,
+	"classes" varchar(500) not null,
+  "components" varchar(50) not null,
+  "concentration" varchar(50) not null,
+  "description" varchar(8000) not null,
+  "duration" varchar(50) not null,
+  "higher_level" varchar(1000),
+  "level" varchar(50) not null,
+  "material" varchar(1000),
+  "page" varchar(50),
+  "range" varchar(500),
+  "ritual" varchar(50),
+  "school" varchar(50),
+  "url" varchar(500)
 );
-
-CREATE TABLE pets (
-	id SERIAL PRIMARY KEY,
-	name varchar(30) not null,
-	breed varchar(50) not null,
-	color varchar(30) not null,
-	owner_id int REFERENCES owners
-);
-
-CREATE TABLE visits (
- id SERIAL PRIMARY KEY,
- check_in_date date DEFAULT NULL,
- check_out_date date DEFAULT NULL,
- pet_id int REFERENCES pets
-);
-
---Dummy data setup
-
-INSERT INTO owners
-VALUES (1, 'Bill', 'Jenkins');
-
-INSERT INTO owners
-VALUES (2, 'Susan', 'Boyle');
-
-INSERT INTO pets
-VALUES (1, 'Sparky', 'black lab', 'black', 1), (2, 'Barksy', 'poodle', 'white', 2);
-
-INSERT INTO visits ("pet_id")
-VALUES (1), (2);
-
-SELECT owner_id, pet_id, first_name, last_name, name, breed, color, check_in_date, check_out_date from owners
-JOIN pets on pets.owner_id = owners.id
-JOIN visits on pets.id = visits.pet_id;
